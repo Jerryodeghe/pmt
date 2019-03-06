4fe4a2e7a205f40e1286cc5e7ce4e5e3760af278
Jerry Odeghe <odeghejerry@gmail.com>
2019-03-06 17:19:09 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './materialize.min.css';
+
+
+class App extends Component {
+  
+  render() {
+    return (
+      <div>
+        <h3>PMT</h3>
+      </div>
+    );
+  }
+}
+
+export default App;
